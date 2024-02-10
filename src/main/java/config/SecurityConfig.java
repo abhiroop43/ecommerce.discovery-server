49b4e936c8090632ee3a6eeefdc52d3d6fe70372
Abhiroop Santra <abhiroop43@users.noreply.github.com>
2024-02-10 10:51:45 +0400

    feat: Add annotations dependency and SecurityConfig class

---
+++ src/main/java/config/SecurityConfig.java
@@ -0,0 +1,55 @@
+//package config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//import static org.springframework.security.config.Customizer.withDefaults;
+//
+//@Configuration
+////@EnableWebSecurity
+//public class SecurityConfig {
+//
+////    @Bean
+////    public static PasswordEncoder passwordEncoder() {
+////        return new BCryptPasswordEncoder();
+////    }
+//
+//    @Bean SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http.csrf(AbstractHttpConfigurer::disable)
+//            .authorizeHttpRequests((authorize) -> {
+//        authorize.anyRequest().permitAll();
+//    }).httpBasic(Customizer.withDefaults());
+//        return http.build();
+//    }
+//
+////    @Bean
+////    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+////        http
+////                .authorizeHttpRequests((auth) -> auth
+////                                               .anyRequest().authenticated()
+////                                      )
+////                .httpBasic(withDefaults());
+////        return http.build();
+////    }
+//
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsService() {
+//        UserDetails user = User.builder()
+//                               .username("eureka")
+//                               .password("{noop}password")
+//                               .roles("USER")
+//                               .build();
+//        return new InMemoryUserDetailsManager(user);
+//    }
+//}
